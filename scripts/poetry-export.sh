c1b9a342f9879b58c36eb33dd5abc9ea52dbd984
Callum Dickinson <callum.karamu@gmail.com>
2023-04-01 03:01:37 +1300

    Install Poetry if it isn't installed

--- scripts/poetry-export.sh
+++ scripts/poetry-export.sh
@@ -7,4 +7,9 @@ set -euo pipefail
 
 root_dir_name=$(basename "$(dirname "$(dirname "${0}")")")
 
+if which poetry > /dev/null 2>&1
+then
+    python3 -m pip install "poetry==1.4.1"
+fi
+
 poetry export -f requirements.txt --without-hashes | sed -E "s,^[^ \t]+ @ file://.*/${root_dir_name}/(.+) ;,\1 ;," > requirements.txt
