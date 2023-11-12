1c5575b91046bb087c9a525a67d6ddd608d8344c
Callum Dickinson <2997843+Callum027@users.noreply.github.com>
2023-11-12 22:09:55 +1300

    Update Buildarr container to v0.7.0 (#18)
    * Add release notes
    * Pull updated submodules
    * Update dependency versions
    * Update Poetry lock files

--- scripts/poetry-export.sh
+++ scripts/poetry-export.sh
@@ -9,7 +9,7 @@ root_dir_name=$(basename "$(dirname "$(dirname "${0}")")")
 
 if ! which poetry > /dev/null 2>&1
 then
-    python3 -m pip install "poetry==1.6.1"
+    python3 -m pip install "poetry==1.7.0" "poetry-plugin-export==1.6.0"
 fi
 
 poetry export -f requirements.txt --without-hashes | sed -E "s,^[^ \t]+ @ file://.*/${root_dir_name}/(.+) ;,\1 ;," > requirements.txt
